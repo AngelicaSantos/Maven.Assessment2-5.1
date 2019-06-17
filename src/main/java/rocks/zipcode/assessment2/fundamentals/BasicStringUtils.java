d927d08de793962c5a507c74468c3f903e09d2ed
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-17 11:24:37 -0400

    basicStringUtils complete

--- src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.assessment2.fundamentals;
 
+import javax.xml.stream.events.Characters;
+import java.util.Arrays;
+import java.util.List;
+
 public class BasicStringUtils {
     /**
      * @param string1 - Base string to be added to
@@ -7,7 +11,8 @@ public class BasicStringUtils {
      * @return concatenation of `string1` and `string2`
      */
     public static String concatentate(String string1, String string2) {
-        return null;
+
+        return string1 + string2;
     }
 
     /**
@@ -15,7 +20,9 @@ public class BasicStringUtils {
      * @return an identical string with characters in reverse order
      */
     public static String reverse(String string1) {
-        return null;
+
+        StringBuilder reversed = new StringBuilder(string1);
+        return reversed.reverse().toString();
     }
 
     /**
@@ -24,7 +31,13 @@ public class BasicStringUtils {
      * @return concatenation of the reverse of `string1` and reverse of `string2`
      */
     public static String reverseThenConcatenate(String string1, String string2) {
-        return null;
+
+        StringBuilder reversed = new StringBuilder(string1);
+        StringBuilder reversed1 = new StringBuilder(string2);
+
+        return reversed.reverse().toString() + reversed1.reverse().toString();
+
+
     }
 
     /**
@@ -33,7 +46,17 @@ public class BasicStringUtils {
      * @return `string` with `charactersToRemove` removed
      */
     public static String removeCharacters(String string, String charactersToRemove) {
-        return null;
+
+        String result = "";
+        String[] characters = charactersToRemove.split("");
+        List<String> list = Arrays.asList(characters);
+        for (int i = 0; i < string.length(); i++) {
+            if (!(list.contains(String.valueOf(string.charAt(i))))) {
+                result += string.charAt(i);
+            }
+
+        }
+        return result;
     }
 
     /**
@@ -42,6 +65,17 @@ public class BasicStringUtils {
      * @return reverse of `string` with `charactersToRemove` removed
      */
     public static String removeCharactersThenReverse(String string, String charactersToRemove) {
-        return null;
+
+        String result = "";
+        String[] characters = charactersToRemove.split("");
+        List<String> list = Arrays.asList(characters);
+        for (int i = 0; i < string.length(); i++) {
+            if (!(list.contains(String.valueOf(string.charAt(i))))) {
+                result += string.charAt(i);
+            }
+
+
+        }
+        return reverse(result);
     }
 }
